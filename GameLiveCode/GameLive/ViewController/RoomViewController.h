0553b15c7383d3e0eb6e845d35e847db43f05329
YaoYouGuan <tarena012@tarena012-yaoyouguang.local>
2016-04-17 17:22:32 +0800

    更新到直播VC加载数据
    …

---
+++ GameLiveCode/GameLive/ViewController/RoomViewController.h
@@ -0,0 +1,15 @@
+//
+//  RoomViewController.h
+//  GameLive
+//
+//  Created by tarena012 on 16/4/17.
+//  Copyright © 2016年 Tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoomViewController : UIViewController
+
+
+
+@end
---
+++ GameLiveCode/GameLive/ViewController/RoomViewController.m
@@ -0,0 +1,25 @@
+//
+//  RoomViewController.m
+//  GameLive
+//
+//  Created by tarena012 on 16/4/17.
+//  Copyright © 2016年 Tarena. All rights reserved.
+//
+
+#import "RoomViewController.h"
+
+@interface RoomViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+
+
+@end
+
+@implementation RoomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+@end
